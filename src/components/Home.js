73d5bcc554b194119e4cc021367db61cae2bddab
Hariom Choudhary <121293840+hariomchoudhary555@users.noreply.github.com>
2023-10-20 21:35:33 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+      <p className="lead">Hello I'm Hariom choudhary.</p>
+      <p class="lead mb-4">
+        Coding Enthusiast - Pursuing B.Tech in Computer Science at VIT Chennai
+      </p>
+    </div>
+  );
+}
+
+export default Home;
